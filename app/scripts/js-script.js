3107239bc63bccf95b8c5d40c8847c9614f95d27
ilmiraA <98748554+ilmiraA@users.noreply.github.com>
2022-10-30 00:04:03 +0300

    Add files via upload

---
+++ app/scripts/js-script.js
@@ -0,0 +1,75 @@
+function superClick(el){
+el.style.backgrouhd ='yellow';
+el.style.color ='black';
+}
+var h3Showed = false;
+function changeDisplay(){
+	const header3 = document.getElementById("1");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
+function changeDisplay1(){
+	const header3 = document.getElementById("2");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
+function changeDisplay2(){
+	const header3 = document.getElementById("3");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
+function SuperClick(){
+	const header3 = document.getElementById("4");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
+function SuperClick1(){
+	const header3 = document.getElementById("5");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
+function SuperClick2(){
+	const header3 = document.getElementById("6");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
+function SuperClick3(){
+	const header3 = document.getElementById("7");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
+function SuperClick4(){
+	const header3 = document.getElementById("8");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
+function SuperClick5(){
+	const header3 = document.getElementById("9");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
+function SuperClick6(){
+	const header3 = document.getElementById("10");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
+function SuperClick7(){
+	const header3 = document.getElementById("11");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
+function SuperClick8(){
+	const header3 = document.getElementById("12");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
+function SuperClick9(){
+	const header3 = document.getElementById("13");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
+function SuperClick10(){
+	const header3 = document.getElementById("14");
+	header3.style.display=h3Showed ? 'block' : 'none';
+	h3Showed = !h3Showed;
+}
